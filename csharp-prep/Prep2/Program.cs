a07af491e70e66bd76b62da35a47e0c0adb30274
logansmith9454 <smi23136@byui.edu>
2025-01-11 21:59:55 -0700

    Completed the first 2 prep assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+        string grade;
+        string gradeMark = "";
+        int gradeMarkIdentitfier = percentage % 10;
+        string message;
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        if (gradeMarkIdentitfier >= 7 && grade != "A" && grade != "F")
+        {
+            gradeMark = "+";
+        }
+        else if (gradeMarkIdentitfier < 3 && grade != "F")
+        {
+            gradeMark = "-";
+        }
+
+        if (percentage < 70)
+        {
+            message = " Try getting help from the tutoring center. Better luck next time!";
+        }
+        else
+        {
+            message = "";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}{gradeMark}.{message}");
     }
 }
