5357270cb1789c09ee5edf92f1729577033fd1cc
= <smi23136@byui.edu>
2025-01-16 14:11:20 -0700

    started prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>;
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            int number = Console.ReadLine();
+            numberList.add(number);
+        } while (number != 0)
+        Console.WriteLine("It is finished");
     }
 }
