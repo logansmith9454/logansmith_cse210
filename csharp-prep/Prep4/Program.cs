0ffc962ba0a243f3a1f0590d0a5aafa4c5216e1c
= <smi23136@byui.edu>
2025-01-16 19:03:36 -0700

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,13 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> numberList = new List<int>;
+        int number = 1;
+        int sum = 0;
+        double average;
+        int largest;
+        List<int> numberList = new List<int>();
         do
         {
-            Console.WriteLine("Enter number: ");
-            int number = Console.ReadLine();
-            numberList.add(number);
-        } while (number != 0)
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+        } while (number != 0);
         Console.WriteLine("It is finished");
+        foreach (int num in numberList)
+        {
+            sum += num;
+        }
+        average = sum / numberList.Count();
+        numberList.Sort();
+        largest = numberList.Last();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
