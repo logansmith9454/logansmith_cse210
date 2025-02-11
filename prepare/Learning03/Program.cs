4e4a6c3d4a21b9a90ea8012fef2ba19de7759633
= <smi23136@byui.edu>
2025-02-11 12:53:05 -0700

    updated the fraction program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -11,7 +11,6 @@ class Program
         Fraction Fraction2 = new Fraction(5);
         Console.WriteLine(Fraction2.GetFractionString());
         Console.WriteLine(Fraction2.GetDecimalValue());
-
         Fraction Fraction3 = new Fraction(3, 4);
         Console.WriteLine(Fraction3.GetFractionString());
         Console.WriteLine(Fraction3.GetDecimalValue());
