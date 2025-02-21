9e15fc0d36e697dffe3925640186f6b0eeee580a
= <smi23136@byui.edu>
2025-02-20 23:36:36 -0700

    added a constructor

--- prepare/Learning04/writingAssignment.cs
+++ prepare/Learning04/writingAssignment.cs
@@ -2,6 +2,10 @@ class WritingAssignment : Assignment
 {
     private string _title;
 
+    public WritingAssignment() : base()
+    {
+        _title = "";
+    }
     public WritingAssignment(string name, string topic, string title) : base(name, topic)
     {
         _title = title;
